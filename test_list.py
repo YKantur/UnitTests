004e471d40311ea35ddb6287fbf7014707d982a9
Yaroslav Kantur <kntryarik@gmail.com>
2025-04-03 21:57:49 +0300

    added failed test to check CI

--- test_list.py
+++ test_list.py
@@ -90,5 +90,9 @@ class TestCircularSinglyLinkedList(unittest.TestCase):
         self.list.clear()
         self.assertEqual(self.list.length(), 0)
 
+    def test_fail_example(self):
+        self.assertEqual(1, 2)  # Завжди падає
+
+
 if __name__ == '__main__':
     unittest.main()
